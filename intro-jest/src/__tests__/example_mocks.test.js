c427fdd89b99baeeab1ab7a1262f3e9199b2641a
Ronald Alves <alvesronald@outlook.com.br>
2021-12-23 08:29:13 -0300

    added more mock examples

--- intro-jest/src/__tests__/example_mocks.test.js
+++ intro-jest/src/__tests__/example_mocks.test.js
@@ -1,18 +1,41 @@
+import { storage } from "../lib/storage";
+import { saveUsername, getUsername } from "../user";
+
+jest.mock("../lib/storage"); // quero simular o conteúdo que tem ali dentro, que me permite utilizar funcoes especificas de mocks / jest
+
 test("first example", () => {
-  const myMock = jest
-    .fn()
-    .mockReturnValueOnce(true)
-    .mockReturnValueOnce("hello world")
-    .mockReturnValueOnce(5);
+  // const myMock = jest
+  //   .fn()
+  //   .mockReturnValueOnce(true)
+  //   .mockReturnValueOnce("hello world")
+  //   .mockReturnValueOnce(5);
+  // const result1 = myMock();
+  // const result2 = myMock();
+  // const result3 = myMock();
+  // // Espera que a func seja chamado 3 vezes
+  // expect(myMock).toHaveBeenCalledTimes(3);
+  // expect(result1).toBe(true);
+  // expect(result2).toBe("hello world");
+  // expect(result3).toBe(5);
+});
 
-  const result1 = myMock();
-  const result2 = myMock();
-  const result3 = myMock();
+test("second example", () => {
+  const username = "john doe";
+  saveUsername(username);
+  expect(storage.save).toHaveBeenCalledTimes(1);
+  expect(storage.save).toHaveBeenCalledWith({
+    key: "username",
+    value: username,
+  });
+});
 
-  // Espera que a func seja chamado 3 vezes
-  expect(myMock).toHaveBeenCalledTimes(3);
+test("third example", () => {
+  const username = "john doe";
+  storage.get.mockReturnValueOnce(username);
 
-  expect(result1).toBe(true);
-  expect(result2).toBe("hello world");
-  expect(result3).toBe(5);
+  const result = getUsername();
+
+  expect(result).toBe(username);
+  expect(storage.get).toHaveBeenCalledTimes(1);
+  expect(storage.get).toHaveBeenCalledWith({ key: "username" });
 });
