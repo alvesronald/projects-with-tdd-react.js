78d22c2e6250177a68201bb4aca4b137732c017e
Ronald Alves <alvesronald@outlook.com.br>
2022-01-03 06:55:33 -0300

    test should exists the fields: name, size, type(eletronic, furniture, clothing)

--- store-app/src/Form/Form.test.js
+++ store-app/src/Form/Form.test.js
@@ -3,11 +3,20 @@ import {render, screen} from '@testing-library/react'
 import {Form} from './Form'
 
 describe('<Form />', () => {
+  beforeEach(() => render(<Form />)) // antes de cada teste, execute a funcao
   it('there must be a create product form page', () => {
-    render(<Form />)
-
     expect(
       screen.queryByRole('heading', {name: /create product/i}),
     ).toBeInTheDocument()
+  })
+
+  it('should exists the fields: name, size, type(eletronic, furniture, clothing)', () => {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/eletronic/i)).toBeInTheDocument()
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
   })
 })
