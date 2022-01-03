179831416b9389848bc0889ef62ec8cdcb0e49ab
Ronald Alves <alvesronald@outlook.com.br>
2022-01-03 06:56:07 -0300

    feat: added components Text Field, Select in form component

--- store-app/src/Form/Form.js
+++ store-app/src/Form/Form.js
@@ -1,5 +1,31 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
 
 export const Form = () => {
-  return <h1>Create product</h1>
+  return (
+    <>
+      <h1>Create product</h1>
+      <form>
+        <TextField label="name" id="name" />
+        <TextField label="size" id="size" />
+
+        <InputLabel htmlFor="type">Type</InputLabel>
+        <Select
+          native
+          value=""
+          inputProps={{
+            name: 'type',
+            id: 'type',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={'eletronic'}>Eletronic</option>
+          <option value={'furniture'}>Furniture</option>
+          <option value={'clothing'}>Clothing</option>
+        </Select>
+      </form>
+    </>
+  )
 }
